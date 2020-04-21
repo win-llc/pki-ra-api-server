c94b47cf7c51a359d3f761158e505d67ca35b504
Davd Winters <jrmints22@gmail.com>
2020-04-20 21:49:03 -0400

    Updates for dynamic keystore location

--- src/main/java/com/winllc/pki/ra/config/AppConfig.java
+++ src/main/java/com/winllc/pki/ra/config/AppConfig.java
@@ -4,16 +4,20 @@ import com.winllc.pki.ra.util.CustomJacksonProvider;
 import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.KeycloakBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 import org.springframework.core.Ordered;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.cors.CorsConfiguration;
@@ -23,15 +27,24 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import javax.annotation.PostConstruct;
 import java.util.Arrays;
 import java.util.Collections;
 
-@SpringBootApplication
+@SpringBootApplication(
+        exclude = {
+                MongoAutoConfiguration.class,
+                MongoDataAutoConfiguration.class
+        }
+)
 @ComponentScan("com.winllc.pki.ra")
 @EntityScan("com.winllc.pki.ra.domain")
 @EnableJpaRepositories(basePackages = "com.winllc.pki.ra.repository")
 @EnableTransactionManagement
 public class AppConfig {
+
+    @Autowired
+    private Environment env;
 
     @Value("${keycloak.admin-interface.server-base-url}")
     private String serverBaseUrl;
@@ -88,6 +101,18 @@ public class AppConfig {
                 .resteasyClient(new ResteasyClientBuilder().connectionPoolSize(10).register(new CustomJacksonProvider()).build())
                 .build();
         return keycloak;
+    }
+
+
+    @PostConstruct
+    private void configureSSL() {
+        //set to TLSv1.1 or TLSv1.2
+        //System.setProperty("https.protocols", "TLSv1.2");
+
+        //load the 'javax.net.ssl.trustStore' and
+        //'javax.net.ssl.trustStorePassword' from application.properties
+        //System.setProperty("javax.net.ssl.trustStore", env.getProperty("server.ssl.trust-store"));
+        //System.setProperty("javax.net.ssl.trustStorePassword",env.getProperty("server.ssl.trust-store-password"));
     }
 
 }
--- src/main/java/com/winllc/pki/ra/keystore/ApplicationKeystore.java
+++ src/main/java/com/winllc/pki/ra/keystore/ApplicationKeystore.java
@@ -1,12 +1,15 @@
 package com.winllc.pki.ra.keystore;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
@@ -23,13 +26,22 @@ public class ApplicationKeystore {
     @Value("${app-keystore.type}")
     private String keystoreType;
 
+    @Autowired
+    @Qualifier("webApplicationContext")
+    private ResourceLoader resourceLoader;
+
+
     private KeyStore ks;
 
     @PostConstruct
     private void load() throws KeyStoreException, IOException, CertificateException, NoSuchAlgorithmException {
+        //Resource resource = resourceLoader.getResource(keystoreLocation);
+        //InputStream input = resource.getInputStream();
+
+        InputStream is = getClass().getResourceAsStream(keystoreLocation);
+
         ks = KeyStore.getInstance(keystoreType);
-        FileInputStream fis = new FileInputStream(keystoreLocation);
-        ks.load(fis, keystorePassword.toCharArray());
+        ks.load(is, keystorePassword.toCharArray());
     }
 
     public KeyStore getKeyStore(){
